724e335d5c20b34bbf6d713f8467c35c99890e8d
David Cha <david.cha44@qmail.cuny.edu>
2019-11-25 22:19:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -5,10 +5,12 @@ end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
